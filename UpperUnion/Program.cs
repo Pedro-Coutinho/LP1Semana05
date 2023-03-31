c168e91fd9fb2f0dfce36a2559614ad093bed1f8
Pedro Coutinho <pedro_coutinho18@outlook.com>
2023-03-31 12:17:55 +0100

    Exercise 9

---
+++ UpperUnion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace UpperUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Check if there's no arguments
+            if (args == null){
+                Console.WriteLine("NO ARGUMENTS");
+                return;
+            }
+            
+            //Sort the array
+            Array.Sort(args);
+
+            //Instantiate String Builder
+            StringBuilder output = new StringBuilder();
+
+            //Cycle through arguments
+            for(int i = 0; i < args.Length ; i++){
+                //Check if it isn't the first argument
+                if(i != 0)
+                    //Append the hyphen
+                    output.Append("-");
+                
+                //Turn the string to upper case
+                args[i] = args[i].ToUpper();
+                //Append the string
+                output.Append(args[i]);
+            }
+
+            //Print the output
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
